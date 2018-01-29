7e98d403c87856caf9836f6d00094cd111ce8f3a
Luc <luc.baracat@hotmail.de>
2018-01-29 17:29:46 +0100

    Changed the structure so that comments get read. Also made an input promt.

--- file_reader.py
+++ file_reader.py
@@ -4,16 +4,27 @@ import string
 class FileReader:
 
     def __init__(self, file_content, create_validation_set):
-        self.chain = self.filter(file_content)
         if create_validation_set:
-            self.training_chain, self.validation_chain = self.split_training_validation(self.chain)
+            self.training_chain, self.validation_chain_list = self.split_training_validation(file_content)
+            self.training_chain = self.filter_for_whole_text(self.training_chain)
+            self.validation_chain_list = self.filter_for_comments(self.validation_chain_list)
+        else:
+            self.chains = self.filter_for_comments(file_content)
 
-    # Calls all other methods of the class to process the given file
-    def filter(self, file_content):
-        return_content = self.remove_punctuation(file_content)
-        return_content = self.remove_upper_case_words(return_content.split())
-        return_content = self.remove_extra_spaces(return_content)
+    # Filters only for the comments
+    def filter_for_comments(self, file_content):
+        return_content = file_content.split("<s>")
+        for i in range (0, len(return_content)):
+            return_content[i] = self.filter_for_whole_text(return_content[i])
         return return_content
+
+    # Currently empty as better validation accuracy is found without preprocessing
+    def filter_for_whole_text(self, file_content):
+        #return_content = self.remove_punctuation(file_content)
+        #return_content = self.remove_upper_case_words(return_content.split())
+        #return_content = self.remove_extra_spaces(return_content)
+        return file_content
+
 
     # Removes all punctuation from the file
     def remove_punctuation(self, file_content):
@@ -31,7 +42,7 @@ class FileReader:
 
     # Splits the content of the file into two part representing 80% and 20% of the original content
     def split_training_validation(self, return_content):
-        train_size = int(len(return_content) * 0.8)
+        train_size = int(len(return_content) * 0.99)
         training_chain = return_content[:train_size]
         validation_chain = return_content[train_size:]
         return training_chain, validation_chain
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def read_training_files():
             file_content = file.read()
             file_reader = FileReader(file_content, create_validation_set=True)
             training_chain_list.append(file_reader.training_chain)
-            validation_chain_list.append(file_reader.validation_chain)
+            validation_chain_list.append(file_reader.validation_chain_list)
 
 
 # Processes the input files into transition matrices
@@ -29,8 +29,40 @@ def read_test_files():
         with open("author_attribution/testikc/" + i, encoding="UTF-8") as file:
             file_content = file.read()
             file_reader = FileReader(file_content, create_validation_set= False)
-            test_chain_list.append(file_reader.chain)
+            test_chain_list.append(file_reader.chains)
 
+
+def predict_all_texts(all_texts, true_names, training_files, verbose, validation):
+    if validation:
+        average_accuracy = 0
+        for i in range (0, len(all_texts)):
+            average_accuracy += predict_text_by_comment(all_texts[i], true_names[i], training_files, verbose, validation)
+        print(average_accuracy / len(all_texts))
+    else:
+        for i in range (0, len(all_texts)):
+            predict_text_by_comment(all_texts[i], None, training_files, True, validation)
+
+def predict_text_by_comment(text_list, true_name, training_files, verbose, validation):
+    total_accuracy = 0
+    for comment in range (0, len(text_list)):
+        max_accuracy = 0
+        best_index = 0
+        for matrix in range (0, len(training_transition_matrix)):
+            tester = Tester(training_transition_matrix[matrix], text_list[comment].split())
+            if tester.accuracy > max_accuracy:
+                max_accuracy = tester.accuracy
+                best_index = matrix
+        if validation:
+            if verbose:
+                print("Most probable Author for Comment", text_list[comment], "is", training_files[best_index], "with ", max_accuracy, "% accuracy, but true author is", true_name)
+            if (training_files[best_index] == true_name):
+                total_accuracy += 1
+        else :
+            if verbose:
+                print("Most probable Author for Comment", text_list[comment], "is", training_files[best_index], "with ",
+                      max_accuracy, "% accuracy")
+
+    return (total_accuracy / len(text_list))
 
 # Tries to guess which author wrote which text.
 # Evaluated are the training matrices against the test texts
@@ -46,6 +78,7 @@ def predict_text(texts, test_name, training_name):
         print("Most Probable Author for Text", test_name[chain], "is", training_name[best_index], ".")
 
 
+
 onlyfiles = [f for f in listdir("author_attribution/trainikc/") if isfile(join("author_attribution/trainikc/", f))]
 onlyfiles_test = [f for f in listdir("author_attribution/testikc/") if isfile(join("author_attribution/testikc/", f))]
 
@@ -56,16 +89,27 @@ validation_chain_list = []
 training_transition_matrix = []
 test_chain_list = []
 
+
 # Performing preprocessing
 read_training_files()
 train_models()
 read_test_files()
 
-# Using all the matrices and chains that we build, we are now trying to guess the author.
-# The test chains are used to test accuracy
-predict_text(validation_chain_list, onlyfiles, onlyfiles)
-predict_text(test_chain_list, onlyfiles_test, onlyfiles)
 
+#The input block to choose between validation and testing mode.
+input_correct = False
+while not input_correct:
+    input = input("If you want to see the validation set accuracy, enter 'v'.\n"
+                  "If you want to see the assigned text for every single comment of every text, press 't'.")
+
+    if (input == 'v'):
+        input_correct = True
+        predict_all_texts(validation_chain_list, onlyfiles, onlyfiles, verbose=False, validation = True)
+    elif (input == 'p'):
+        input_correct = True
+        predict_all_texts(test_chain_list, None, onlyfiles, verbose=True, validation=False)
+    else:
+        input ("Input not correct. Please try again.")
 
 
 
